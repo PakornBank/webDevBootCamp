fe937f1821da2bf2de9f7cccb0600586142dd2a0
PakornBank <pakorn22120@gmail.com>
2023-12-10 01:14:43 +0700

    Express example code.

---
+++ node/FirstApp/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+
+// app.use((req, res) => {
+//   console.log("We got a request!");
+//   res.send("<h1>This is my web page</h1>");
+// });
+
+app.get("/", (req, res) => {
+  res.send("This is the home page.");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("MEOW!");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("WOOF!");
+});
+
+app.post("/cats", (req, res) => {
+  res.send("You requested something?");
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.send(`<h1>You requested ${subreddit} subreddit</h1>`);
+});
+
+app.get("/r/:subreddit/:postId", (req, res) => {
+  const { subreddit, postId } = req.params;
+  res.send(`<h1> Your requested post ${postId} of r/${subreddit}`);
+});
+
+app.get("/search", (req, res) => {
+  const { q } = req.query;
+  if (!q) res.send("<h1>Nothing found if nothing searched</h1>");
+  res.send(`<h1>search result for ${q}</h1>`);
+});
+
+app.get("*", (req, res) => {
+  res.send("I don't know that path");
+});
+
+app.post("*", (req, res) => {
+  res.send("มั่วละ");
+});
+
+app.listen(3000, () => {
+  console.log("LISTENING ON PORT 3000");
+});
