f3340952957349c58d4eedbe620d2daf9cbf96df
PakornBank <pakorn22120@gmail.com>
2023-12-16 19:38:00 +0700

    back-end learning codes.

--- back-end/index.js
+++ back-end/index.js
@@ -3,9 +3,11 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const morgan = require("morgan");
 
+const AppError = require("./AppError");
 const Product = require("./models/product");
-
+// love bank
 mongoose
   .connect("mongodb://127.0.0.1:27017/farmStand")
   .then(() => {
@@ -18,28 +20,103 @@ mongoose
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
+app.use(morgan("tiny"));
 
-app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  res.send(products);
-});
+// app.use((req, res, next) => {
+//   console.log("This is my first middleware.");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log("This is my second middleware.");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   req.requestTime = Date.now();
+//   console.log(req.method, req.path, req.params, req.query);
+//   next();
+// });
 
-app.get("/products/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.send(product);
-});
+function wrapAsync(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((err) => next(err));
+  };
+}
 
-app.post("/products", async (req, res) => {
-  try {
+app.get(
+  "/products",
+  wrapAsync(async (req, res, next) => {
+    console.log(req.requestTime);
+    if (req.query) {
+      const products = await Product.find({ category: req.query.category });
+      res.send(products);
+    } else {
+      const products = await Product.find({});
+      res.send(products);
+    }
+  })
+);
+
+app.get(
+  "/products/:id",
+  wrapAsync(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return next(new AppError("Product Not Found", 404));
+    }
+    res.send(product);
+  })
+);
+
+// app.get("/fly", (req, res) => {
+//   throw new AppError("UNACCEPTABLE!", 406);
+// });
+
+app.post(
+  "/products",
+  wrapAsync(async (req, res, next) => {
     const product = new Product(req.body);
     const savedProduct = await product.save();
     res.send(savedProduct);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+  })
+);
+
+app.put(
+  "/products/:id",
+  wrapAsync(async (req, res, next) => {
+    const updated = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    res.send(updated);
+  })
+);
+
+app.delete(
+  "/products/:id",
+  wrapAsync(async (req, res, next) => {
+    const deleted = await Product.findByIdAndDelete(req.params.id);
+    res.send(deleted);
+  })
+);
+
+app.use((req, res) => {
+  res.status(404).send("Oops, Not found");
 });
 
-app.put("/products/:id", async (req, res) => {});
+const handleValidationError = (err) => {
+  return new AppError(`Validation Failed...${err.message}`, 400);
+};
+
+app.use((err, req, res, next) => {
+  console.log(err.name);
+  if (err.name === "ValidationError") return next(handleValidationError(err));
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "ERROR" } = err;
+  res.status(status).send(message);
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
